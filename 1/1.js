a18feeafcdf95fd35dad63aed406e1cd7f5a51bb
Joe Cullin <jcullin@thomasnet.com>
2019-12-01 08:49:23 -0500

    Day 1 solutions.

--- 1/1.js
+++ 1/1.js
@@ -1,6 +1,18 @@
 #!/usr/local/bin/node
 
 const logger = require("../common/logger");
+const readInput = require("../common/readInput");
 
+const calculateFuel = mass => {
+  const fuel = Math.floor(mass / 3) - 2;
+  logger.debug(`${mass} --> ${fuel}`);
+  return fuel;
+};
 
-logger.info("I'm ready!");
+readInput().then(input => {
+  const total = input.reduce((accumulator, currentValue) => {
+    return accumulator + calculateFuel(currentValue);
+  }, 0);
+
+  logger.info("total: " + total);
+});
---
+++ 1/2.js
@@ -0,0 +1,32 @@
+#!/usr/local/bin/node
+
+const logger = require("../common/logger");
+const readInput = require("../common/readInput");
+
+const calculateFuel = mass => {
+  const fuel = Math.floor(mass / 3) - 2;
+  return fuel > 0 ? fuel : 0;
+};
+
+const calculateExtraFuel = fuelMass => {
+  logger.debug(`calculateExtraFuel ${fuelMass}`);
+  let extraFuel = calculateFuel(fuelMass);
+  if (extraFuel > 0) {
+    extraFuel += calculateExtraFuel(extraFuel);
+  }
+  return extraFuel;
+};
+
+readInput().then(input => {
+  const total = input.reduce((accumulator, moduleMass) => {
+    const moduleFuel = calculateFuel(moduleMass);
+    logger.debug(`Initial ${moduleMass} --> ${moduleFuel}`);
+
+    const extraFuel = calculateExtraFuel(moduleFuel);
+    logger.debug(`---> extra ${extraFuel}`);
+
+    return accumulator + moduleFuel + extraFuel;
+  }, 0);
+
+  logger.info("total: " + total);
+});
