19e777b79c95bcad4a1c4d79f626d930cfc1c401
Joe Cullin <jcullin@thomasnet.com>
2019-12-03 08:59:16 -0500

    Day 3 solutions.

--- 3/2.js
+++ 3/2.js
@@ -5,12 +5,15 @@ const { readCommaSeparatedLines } = require("../common/readInput");
 
 const traceWire = pathInstructions => {
   let pathTraced = [];
+  let totalSteps = 0;
+  let stepCounts = {};
   let currentPoint = [0, 0];
   pathInstructions.forEach(instruction => {
     logger.debug(`instruction: ${instruction}`);
     const direction = instruction.substring(0, 1);
     let steps = instruction.substring(1);
     while (steps > 0) {
+      totalSteps++;
       steps--;
       let x = 0;
       let y = 0;
@@ -33,25 +36,27 @@ const traceWire = pathInstructions => {
       let nextPoint = [...currentPoint];
       nextPoint[0] += x;
       nextPoint[1] += y;
-      pathTraced.push(nextPoint.join(":"));
+      const pointString = nextPoint.join(":");
+      pathTraced.push(pointString);
+      if (!stepCounts[pointString]) {
+        stepCounts[pointString] = totalSteps;
+      }
       currentPoint = nextPoint;
       logger.debug({ message: "point", currentPoint });
     }
   });
-  return pathTraced;
+  return { pathTraced: pathTraced, stepCounts: stepCounts };
 };
 
 readCommaSeparatedLines().then(input => {
   logger.debug({ message: "input", input });
-  logger.debug("wire 1 -----------------------------");
-  const path1 = traceWire(input[0]);
-  logger.debug("wire 2 -----------------------------");
-  const path2 = traceWire(input[1]);
-  logger.debug({ message: "wire 1 path", path1 });
-  logger.debug({ message: "wire 2 path", path2 });
+  const wire1 = traceWire(input[0]);
+  const wire2 = traceWire(input[1]);
+  logger.debug({ message: "wire 1", wire1 });
+  logger.debug({ message: "wire 2", wire2 });
 
-  let points1 = new Set(path1);
-  let points2 = new Set(path2);
+  let points1 = new Set(wire1.pathTraced);
+  let points2 = new Set(wire2.pathTraced);
   logger.debug({ message: "wire 1 points", values: Array.from(points1) });
   logger.debug({ message: "wire 2 points", values: Array.from(points2) });
   let intersections = [...points1].filter(x => points2.has(x));
@@ -59,8 +64,9 @@ readCommaSeparatedLines().then(input => {
 
   const distances = intersections
     .map(point => {
-      let coords = point.split(":");
-      return Math.abs(coords[0]) + Math.abs(coords[1]);
+      const steps1 = wire1.stepCounts[point];
+      const steps2 = wire2.stepCounts[point];
+      return steps1 + steps2;
     })
     .sort((a, b) => {
       return a - b;
