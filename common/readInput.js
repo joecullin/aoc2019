72c9fe9c431a9ed7859e84811e599b998d03d109
Joe Cullin <jcullin@thomasnet.com>
2019-12-06 17:57:35 -0500

    yet another input utility.

--- common/readInput.js
+++ common/readInput.js
@@ -3,22 +3,36 @@ const getStdin = require("get-stdin");
 const readLinesNumeric = () => {
   return getStdin().then(input => {
     return input.split("\n").map(val => parseInt(val));
-    resolve();
+  });
+};
+
+const readLines = () => {
+  return getStdin().then(input => {
+    return input.split("\n");
   });
 };
 
 const readCommaSeparatedLineNumeric = () => {
   return getStdin().then(input => {
-    return input.trim().split(",").map(val => parseInt(val));
-    resolve();
+    return input
+      .trim()
+      .split(",")
+      .map(val => parseInt(val));
   });
 };
 
 const readCommaSeparatedLines = () => {
   return getStdin().then(input => {
-    return input.trim().split("\n").map(line => line.split(","));
-    resolve();
+    return input
+      .trim()
+      .split("\n")
+      .map(line => line.split(","));
   });
 };
 
-module.exports = {readLinesNumeric, readCommaSeparatedLineNumeric, readCommaSeparatedLines};
+module.exports = {
+  readLinesNumeric,
+  readCommaSeparatedLineNumeric,
+  readCommaSeparatedLines,
+  readLines
+};
