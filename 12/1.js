026fff12acaf41ff135da442c9e4d5d6ab4b13c6
Joe Cullin <jcullin@thomasnet.com>
2019-12-13 18:30:31 -0500

    Day 12 part 1 solution.

--- 12/1.js
+++ 12/1.js
@@ -6,24 +6,59 @@ const readMap = require("./readMap");
 const { printMap } = require("./printMap");
 
 const applyGravity = ({ mapData }) => {
-  let adjustedMap = [...mapData];
+  const axes = ["x", "y", "z"];
+  const allPositions = [
+    mapData.map(moon => moon.position.x),
+    mapData.map(moon => moon.position.y),
+    mapData.map(moon => moon.position.z)
+  ];
 
-  // for each axis
-    // collect all the moon's mapData values on that axis
-    // for each moon A
-      // for each other moon B
-        // adjust A's velocity based on comparing A vs B values
+  // update velocities
+  mapData.forEach((moon, moonIndex) => {
+    axes.forEach((axis, axisIndex) => {
+      let thisPosition = allPositions[axisIndex][moonIndex];
+      let velocityChange = 0;
+      allPositions[axisIndex]
+        .filter((position, positionIndex) => positionIndex !== moonIndex)
+        .forEach(otherMoonPosition => {
+          if (otherMoonPosition !== thisPosition) {
+            velocityChange += otherMoonPosition < thisPosition ? -1 : 1;
+          }
+        });
+      // logger.debug(`moon #${moonIndex} ${axis} axis velocity change: ${velocityChange}`);
+      mapData[moonIndex].velocity[axis] += velocityChange;
+    });
+  });
 
+  // apply velocities
+  mapData.forEach((moon, moonIndex) => {
+    axes.forEach(axis => {
+      mapData[moonIndex].position[axis] += mapData[moonIndex].velocity[axis];
+    });
+  });
 
+  return mapData;
+};
 
+const moonPotential = moon => {
+  const potential =
+    Math.abs(moon.position.x) +
+    Math.abs(moon.position.y) +
+    Math.abs(moon.position.z);
+  return potential;
+};
 
-
-  return adjustedMap;
+const moonKinetic = moon => {
+  const kinetic =
+    Math.abs(moon.velocity.x) +
+    Math.abs(moon.velocity.y) +
+    Math.abs(moon.velocity.z);
+  return kinetic;
 };
 
 readLines().then(input => {
   const inputMap = readMap({ input });
-  const steps = 3;
+  const steps = 1000;
 
   let currentMap = [...inputMap];
   for (let step = 1; step <= steps; step++) {
@@ -32,5 +67,10 @@ readLines().then(input => {
     printMap({ mapData: currentMap });
   }
 
+  const totalEnergy = currentMap.reduce((acc, moon) => {
+    return acc + moonPotential(moon) * moonKinetic(moon);
+  }, 0);
+
+  logger.info(`total energy: ${totalEnergy}`);
   logger.debug("done");
 });
--- 12/readMap.js
+++ 12/readMap.js
@@ -3,14 +3,14 @@ const readMap = ({ input }) => {
   input.forEach(line => {
     let moon = {
       position: { x: null, y: null, z: null },
-      velocity: { x: null, y: null, z: null }
+      velocity: { x: 0, y: 0, z: 0 }
     };
     line
       .replace(/[>< ]/g, "")
       .split(",")
       .forEach(coordinate => {
         const [axis, value] = coordinate.split(/[=]/);
-        moon.position[axis] = value;
+        moon.position[axis] = parseInt(value);
       });
     mapData.push(moon);
   });
