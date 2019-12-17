cb83fce55cba2993ce8b7b5fa247b12654a681fe
Joe Cullin <jcullin@thomasnet.com>
2019-12-16 23:04:12 -0500

    Day 12 part 2 solution, finally.

--- 12/2.js
+++ 12/2.js
@@ -3,69 +3,18 @@
 const logger = require("../common/logger-simple");
 const { readLines } = require("../common/readInput");
 const readMap = require("./readMap");
-const sleep = require("../common/sleep");
-var levelup = require("levelup");
-var leveldown = require("leveldown");
+const mathjs = require("mathjs");
 
-const maxSteps = 4686774924;
-// const maxSteps = 10000;
 const axes = ["x", "y", "z"];
-let theMap = {};
-let allPositions = [];
-let step = 1;
-let thisPosition;
-let velocityChange;
-
-let seenBuckets = {};
-let seen = [];
-let seenKey = "";
-let keyValues = [];
-
-// Generate shorter (but still unique) array keys.
-// I only need to go one-way, so I took shortcuts with chunks & ordering.
-const alphabet =
-  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const toRange = alphabet.split("");
-const toBase = toRange.length;
-const fromRange = toRange.slice(0, 12);
-const packKey = () => {
-  seenKey = "";
-  keyValues.forEach(val => {
-    if (val < 0) {
-      seenKey += "a";
-      seenKey += Math.abs(val).toString();
-    } else {
-      seenKey += "b";
-      seenKey += val.toString();
-    }
-  });
-
-  let keyChunks = [];
-  for (let i = 16; i < seenKey.length; i += 16) {
-    keyChunks.push(seenKey.slice(i - 16, i));
-  }
-  keyChunks.push(seenKey.slice(seenKey.length - (16 - (seenKey.length % 16)))); // last fragment
-
-  seenKey = "";
-  keyChunks.forEach(chunk => {
-    let decimal = 0;
-    decimal = chunk.split("").reduce(function(acc, digit, index) {
-      return (acc += fromRange.indexOf(digit) * Math.pow(12, index));
-    }, 0);
-
-    while (decimal > 0) {
-      seenKey += toRange[decimal % toBase];
-      decimal = (decimal - (decimal % toBase)) / toBase;
-    }
-  });
-};
 
 readLines().then(async input => {
-  theMap = readMap({ input });
-  let foundRepeat = false;
+  let theMap = readMap({ input });
+  let originalMap = readMap({ input });
 
-  while (step <= maxSteps && !foundRepeat) {
-    allPositions = [
+  let foundLoop = { x: 0, y: 0, z: 0 };
+  let step = 1;
+  while (foundLoop.x === 0 || foundLoop.y === 0 || foundLoop.z === 0) {
+    let allPositions = [
       theMap.map(moon => moon.position.x),
       theMap.map(moon => moon.position.y),
       theMap.map(moon => moon.position.z)
@@ -74,8 +23,8 @@ readLines().then(async input => {
     // update velocities
     theMap.forEach((moon, moonIndex) => {
       axes.forEach((axis, axisIndex) => {
-        thisPosition = allPositions[axisIndex][moonIndex];
-        velocityChange = 0;
+        let thisPosition = allPositions[axisIndex][moonIndex];
+        let velocityChange = 0;
         allPositions[axisIndex]
           .filter((position, positionIndex) => positionIndex !== moonIndex)
           .forEach(otherMoonPosition => {
@@ -94,49 +43,24 @@ readLines().then(async input => {
       });
     });
 
-    // generate unique key
-    keyValues = [];
-    theMap.forEach((moon, moonIndex) => {
-      axes.forEach(axis => {
-        keyValues.push(theMap[moonIndex].position[axis]);
-        keyValues.push(theMap[moonIndex].velocity[axis]);
-      });
-    });
-    packKey();
-
-    const bucketId = seenKey.substring(0, 2);
-    try {
-      seen = levelup(leveldown("./data/db" + bucketId));
-    } catch (err) {
-      logger.info(`Error creating/opening bucket '${bucketId}`, err);
-    }
-
-    if (seenBuckets[bucketId]) {
-      try {
-        await seen.get(seenKey);
-        logger.info(`repeated state at step #${step}! for ${seenKey}`);
-        foundRepeat = true;
-      } catch (err) {
-        // We expect lots of notFound errors. Only report other errors.
-        if (!err.notFound) {
-          logger.info(`GET error. step=${step} key=${seenKey}`, err);
-        }
+    // The trick:
+    //  - each axis is unaffected by the other axes, and
+    //  - each axis has a repeating pattern.
+    // So we count when each axis first encounters an already-seen state.
+    // (i.e. when it's back to its starting point.)
+    // Then find the least common multiple of all 3 counts.
+    axes.forEach(axis => {
+      if (
+        !foundLoop[axis] &&
+        theMap.every(
+          (moon, moonIndex) =>
+            moon.position[axis] === originalMap[moonIndex].position[axis] &&
+            moon.velocity[axis] === originalMap[moonIndex].velocity[axis]
+        )
+      ) {
+        foundLoop[axis] = step;
       }
-    } else {
-      seenBuckets[bucketId] = true;
-    }
-
-    try {
-      await seen.put(seenKey, "");
-    } catch (err) {
-      logger.info(`error in put for '${seenKey}'`, err);
-    }
-
-    try {
-      await seen.close();
-    } catch (err) {
-      logger.info(`error in close for '${seenKey}'`, err);
-    }
+    });
 
     if (step % 100000 === 0) {
       logger.debug(`step ${step} (` + step.toString().length + ` digits)`);
@@ -146,8 +70,12 @@ readLines().then(async input => {
   }
 
   logger.debug(
-    `pausing at step ${step} (` + step.toString().length + ` digits)`
+    `stopped at ${step} (` + step.toString().length + ` digits)`,
+    foundLoop
   );
+
+  const answer = mathjs.lcm(foundLoop.x, foundLoop.y, foundLoop.z);
+  logger.info(`answer: ${answer}`);
 
   // await sleep(10 * 60 * 1000);
   logger.debug("done");
